fa1c26d3cd40446a59aacecb8019fec8a73b59a9
2Prerna0 <prernaprashar@7170.com>
2025-04-03 13:35:37 +0530

    logger and exception file

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+from src.logger import logging
+from src.exception import CustomException
+import os, sys
+app = Flask(__name__)
+
+@app.route('/',methods= ['GET', 'POST'])
+def index():
+    try:
+        logging.info("we are testing our second methods of logging")
+        return "Welcome to Engineering wala bhaiya ML project Session"
+    except Exception as e:
+        abc = CustomException(e, sys)
+        logging.info(abc.error_message)
+        return "Welcome to My happy Place"
+        
+if __name__ == "__main__":
+    app.run(debug=True)
