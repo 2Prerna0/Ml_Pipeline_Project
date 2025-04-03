361d9b520708b942f8294b8d6a16064567449101
2Prerna0 <prernaprashar@7170.com>
2025-04-03 23:26:08 +0530

    data ingestion completed

---
+++ src/component/data_ingestion.py
@@ -0,0 +1,54 @@
+import os, sys 
+import pandas as pd 
+import numpy as np 
+from src.logger import logging 
+from src.exception import CustomException
+from dataclasses import dataclass 
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join("artifacts", "train.csv")
+    test_data_path = os.path.join("artifacts", "test.csv")
+    raw_data_path = os.path.join("artifacts", "raw.csv")
+    
+# C:\Users\prern\Desktop\PrOjEcTs\Ml_Pipeline_Project\notebook\Data\income_cleandata.csv
+
+class DataIngestion:
+    def __init__(self):
+        logging.info("Data ingestion Started")
+        self.ingestion_config = DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        try:
+            logging.info("Data Reading using Pandas Library from local system")
+            data = pd.read_csv(os.path.join("notebook","Data","income_cleandata.csv"))
+            logging.info("Data Reading completed")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index = False)
+           
+            logging.info("Data splitted into train and test")
+            train_set, test_set = train_test_split(data,test_size = 0.30, random_state= 42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False ,header =True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True) 
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e: 
+            logging.info("Erro occured in data ingestion stage")
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":    
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
+        
+        #  notebook\Data\income_cleandata.csv  
+        # src\component\data_ingestion.py 
+        
+        
+        
